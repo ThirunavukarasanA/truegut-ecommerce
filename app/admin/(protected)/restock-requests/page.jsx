75c56a1e0bd50b4c8c8a6a7a07078ed930bf6ef6
ThirunavukarasanA <thirunavukarasan@rvsgroup.com>
2026-01-06 12:32:49 +0530

    @/components/admin/common/AdminTable now fixed

--- app/admin/(protected)/restock-requests/page.jsx
+++ app/admin/(protected)/restock-requests/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import AdminPageHeader from "@/components/Admin/common/AdminPageHeader";
+import AdminPageHeader from "@/components/admin/common/AdminPageHeader";
 // import AdminTable from "@/components/admin/common/AdminTable";
 import AdminTable from "@/components/admin/common/AdminTable";
 import { toast } from "react-hot-toast";
